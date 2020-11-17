df5e6f9630af711300304501e4d8d21e5b3617dd
richard.brown@audacia.co.uk <richard.brown@audacia.co.uk>
2020-11-17 15:41:16 +0000

    Added ValidatedNotNullAttribute, plus some small rule changes

--- Analyzers/Audacia.CodeAnalysis.Analyzers.Helpers/src/Audacia.CodeAnalysis.Analyzers.Helpers/ParameterCount/MaxParameterCountAttribute.cs
+++ Analyzers/Audacia.CodeAnalysis.Analyzers.Helpers/src/Audacia.CodeAnalysis.Analyzers.Helpers/ParameterCount/MaxParameterCountAttribute.cs
@@ -8,8 +8,21 @@ namespace Audacia.CodeAnalysis.Analyzers.Helpers.ParameterCount
     [AttributeUsage(AttributeTargets.Method | AttributeTargets.Constructor)]
     public sealed class MaxParameterCountAttribute : Attribute
     {
+        /// <summary>
+        /// Gets the maximum number of parameters allowed.
+        /// </summary>
         public int ParameterCount { get; }
 
+        /// <summary>
+        /// Gets or sets a justification explaining why the default value has been overridden.
+        /// This is optional, but it can be useful if there are external constraints restricting what can be done to the code in question.
+        /// </summary>
+        public string Justification { get; set; }
+
+        /// <summary>
+        /// Initializes an instance of <see cref="MaxParameterCountAttribute"/>, with the number of parameters allowed set to <paramref name="parameterCount"/>.
+        /// </summary>
+        /// <param name="parameterCount">The number of parameters allowed in the decorated method.</param>
         public MaxParameterCountAttribute(int parameterCount)
         {
             ParameterCount = parameterCount;
