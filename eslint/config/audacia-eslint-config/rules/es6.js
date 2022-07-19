e60288f9cc8259c44c1f60f54305239b9b50f9a2
Richard Brown <richard.brown@audacia.co.uk>
2022-07-19 09:05:27 +0100

    feat: disallow import of rxjs and lodash with typescript rule #116965

--- eslint/config/audacia-eslint-config/rules/es6.js
+++ eslint/config/audacia-eslint-config/rules/es6.js
@@ -3,8 +3,9 @@ module.exports = {
   {
     // disallow specific imports
     // https://eslint.org/docs/rules/no-restricted-imports
-    'no-restricted-imports': ['off', {
-      paths: ['rxjs'],
+    'no-restricted-imports': 'off',
+    '@typecript-eslint/no-restricted-imports': ['error', {
+      paths: ['rxjs', 'lodash'],
       patterns: []
     }]
   }
