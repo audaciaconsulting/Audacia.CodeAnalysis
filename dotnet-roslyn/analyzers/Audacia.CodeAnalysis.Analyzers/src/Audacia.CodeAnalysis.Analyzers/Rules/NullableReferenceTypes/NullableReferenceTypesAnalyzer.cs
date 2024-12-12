645215847915ebcba03dd245b3e40b094cea01b4
tom.shephard <tom.shephard@audacia.co.uk>
2024-12-12 15:09:37 +0000

    fix!: Attempt 3: Tried to set it so that it only highlights the class declaration line at least #183005

--- dotnet-roslyn/analyzers/Audacia.CodeAnalysis.Analyzers/src/Audacia.CodeAnalysis.Analyzers/Rules/NullableReferenceTypes/NullableReferenceTypesAnalyzer.cs
+++ dotnet-roslyn/analyzers/Audacia.CodeAnalysis.Analyzers/src/Audacia.CodeAnalysis.Analyzers/Rules/NullableReferenceTypes/NullableReferenceTypesAnalyzer.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Immutable;
+using System.Linq;
 using Audacia.CodeAnalysis.Analyzers.Common;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
@@ -50,32 +51,30 @@ namespace Audacia.CodeAnalysis.Analyzers.Rules.NullableReferenceTypes
                 analysisContext.RegisterSemanticModelAction(AnalyzeNullableContextOptions));
         }
 
-        /// <summary>
-        /// Evaluates whether nullable options are enabled on the given <paramref name="context"/>.
-        /// If not, the diagnostic rule is triggered.
-        /// </summary>
-        /// <param name="context">The analysis context.</param>
         private static void AnalyzeNullableContextOptions(SemanticModelAnalysisContext context)
         {
-            var nullableContextOptions = context.SemanticModel
-                .Compilation
-                .Options
-                .NullableContextOptions;
+            var compilation = context.SemanticModel.Compilation;
+            var nullableContextOptions = compilation.Options.NullableContextOptions;
 
-            // Allow all possible options, excluding 'Disable' (which is the assumed value if the node is missing altogether).
+            // If nullable reference types are enabled, no diagnostic is reported.
             if (nullableContextOptions != NullableContextOptions.Disable)
             {
                 return;
             }
 
-            var location = context.SemanticModel
-                .SyntaxTree
-                .GetRoot()
-                .GetLocation();
+            // Identify the class declaration node of this analyzer class.
+            var root = context.SemanticModel.SyntaxTree.GetRoot(context.CancellationToken);
+            var classDeclaration = root
+                .DescendantNodes()
+                .OfType<Microsoft.CodeAnalysis.CSharp.Syntax.ClassDeclarationSyntax>()
+                .FirstOrDefault(cd => cd.Identifier.ValueText == nameof(NullableReferenceTypesAnalyzer));
+
+            // If we found the class declaration, use its location; otherwise, fallback to the file root.
+            var location = classDeclaration?.GetLocation() ?? root.GetLocation();
 
             var diagnostic = Diagnostic.Create(Rule, location);
-
             context.ReportDiagnostic(diagnostic);
         }
+
     }
 }
