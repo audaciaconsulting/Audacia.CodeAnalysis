8ad1f8b368d39affc5d0b3082ea3b0fe469de112
thomas-cooper-audacia <tom.cooper@audacia.co.uk>
2024-12-12 10:30:42 +0000

    feat: add extension methods for reading aliases from using statements #183011

---
+++ dotnet-roslyn/analyzers/Audacia.CodeAnalysis.Analyzers/src/Audacia.CodeAnalysis.Analyzers/Extensions/CompilationExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+
+namespace Audacia.CodeAnalysis.Analyzers.Extensions
+{
+    internal static class CompilationExtensions
+    {
+        private static Tuple<WeakReference<Compilation>, ConcurrentDictionary<SyntaxTree, bool>> usingAliasCache
+            = Tuple.Create(
+                new WeakReference<Compilation>(null), 
+                default(ConcurrentDictionary<SyntaxTree, bool>));
+
+        public static ConcurrentDictionary<SyntaxTree, bool> GetOrCreateUsingAliasCache(this Compilation compilation)
+        {
+            var cache = usingAliasCache;
+
+            Compilation cachedCompilation;
+            if (!cache.Item1.TryGetTarget(out cachedCompilation) || cachedCompilation != compilation)
+            {
+                var replacementCache = Tuple.Create(new WeakReference<Compilation>(compilation), new ConcurrentDictionary<SyntaxTree, bool>());
+                while (true)
+                {
+                    var prior = Interlocked.CompareExchange(ref usingAliasCache, replacementCache, cache);
+                    if (prior == cache)
+                    {
+                        cache = replacementCache;
+                        break;
+                    }
+
+                    cache = prior;
+                    if (cache.Item1.TryGetTarget(out cachedCompilation) && cachedCompilation == compilation)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            return cache.Item2;
+        }
+    }
+}
